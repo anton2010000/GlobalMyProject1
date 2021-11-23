42f0223406ac0e76687704793ef01bd0fa2070fc
Anton Fti <89021397725@mail.ru>
2021-11-23 18:40:10 +0300

    add function MyMul()

--- program.cpp
+++ program.cpp
@@ -1,13 +1,12 @@
-//файл program.cpp
-
+﻿//файл program.cpp
 #include <iostream>
 #include "funcs.h"
 using namespace std;
 int main()
 {
-cout << MySum(10, 3) << endl;
-cout << MySub(10, 3) << endl;
-system("pause");
-
-return 0;
+	cout << MySum(10, 3) << endl;
+	cout << MySub(10, 3) << endl;
+	cout << MyMul(10, 3) << endl;
+	system("pause");
+	return 0;
 }
